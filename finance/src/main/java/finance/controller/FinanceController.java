1c95cd043701d0f04d9e4eb404eda0f4b618c0a3
lisn0 <lisnoxvel@gmail.com>
2022-02-02 00:20:49 +0100

    added ingress

--- finance/src/main/java/finance/controller/FinanceController.java
+++ finance/src/main/java/finance/controller/FinanceController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/finance")
+@RequestMapping("api/v1/finance")
 @RequiredArgsConstructor
 public class FinanceController {
 
